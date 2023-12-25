be547c7618caf08d415c679e14e4d41cb96e2ea7
Adityapadmanair <adityapnair15@gmail.com>
2023-12-25 02:04:33 +0100

    To do list app Initial commit

---
+++ actions.js
@@ -0,0 +1,64 @@
+var input = document.getElementById("userinput");
+var button = document.getElementById("enter");
+var ul = document.querySelector("ul")
+var firstbutton = document.getElementById("firstbutton")
+
+function inputLength()
+{
+	return input.value.length;
+}
+
+function checkIfThere()
+{
+	for(var i=0; i< ul.children.length; i++)
+	{
+		 if (input.value === (ul.children[i].textContent))
+		 {
+		 	
+		 	input.value= ""
+		 	alert("already there")
+		 	return
+		 }
+	}
+
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value ="";	
+}
+
+
+function createListElement()
+{			
+			checkIfThere();
+}
+
+function addListClick()
+{
+	if(inputLength()>0)
+	{
+			createListElement();
+	}
+}
+
+function addListEnter(event)
+{
+	var which= event.which
+	if(inputLength() >0 && which === 13)
+	{
+			createListElement();
+	}
+}
+
+function toggleState(button) {
+    button.parentNode.classList.toggle("done");
+}
+
+
+button.addEventListener("click", addListClick);
+
+input.addEventListener("keydown", addListEnter); 
+
+firstbutton.addEventListener("click", toggleState)
+	
+
