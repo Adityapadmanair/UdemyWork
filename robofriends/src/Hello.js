9c92a7f50e104cc94c3bddceb1043d98164f8c02
Adityapadmanair <adityapnair15@gmail.com>
2024-01-02 03:09:48 +0100

    Made a new component hello

---
+++ robofriends/src/Hello.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './hello.css'
+
+class Hello extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>
+                    Eren Yaegerrrrr
+                </h1>
+                <p>
+                    Wassup annoi
+                </p>
+            </div>
+        );
+    }
+}
+export default Hello;
--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-  <h1> Eren Yaeger </h1>
-    <App />
+  <React.StrictMode> 
+    <Hello />
   </React.StrictMode>
 );
 
