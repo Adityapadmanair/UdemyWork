6592fe0dd0f43aec678781764896de62d7b1ecf1
Adityapadmanair <adityapnair15@gmail.com>
2024-01-03 03:09:23 +0100

    Done with Cardlist and Card components now creating searchbox

---
+++ robofriends/src/Searchbox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Searchbox = () => {
+    return(
+        <div className='pa2'>
+            <input className='pa3 ba b--green bg-lightesh-blue'
+             type = 'search' placeholder ='search for your robots here'
+             />
+        </div>
+    );
+}
+
+export default Searchbox;
