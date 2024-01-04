fafb695907556c6a490d2b2a34b16338eb15369c
Adityapadmanair <adityapnair15@gmail.com>
2024-01-04 02:46:33 +0100

    Created a working searchbar

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import Searchbox from './Searchbox';
 
 
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>          Robofriends           </h1>
-            < Searchbox />
-            <CardList robots= {robots}/>
+class App extends Component {
+    constructor(){
 
-        </div>
+        super()
+        this.state = {
+            robots: robots,
+             searchfield: ''
+         }
+    }
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+        
+    }
 
-        );
+
+
+    render(){
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        return (
+            <div className='tc'>
+                <h1>          Robofriends           </h1>
+                <Searchbox searchChange = {this.onSearchChange}/>
+                <CardList robots= {filteredRobots}/>
+            </div>
+    
+            );
+    }
+
 }
+
 
 export default App;
--- robofriends/src/Searchbox.js
+++ robofriends/src/Searchbox.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Searchbox = () => {
+const Searchbox = ({searchfield , searchChange}) => {
     return(
         <div className='pa2'>
             <input className='pa3 ba b--green bg-lightesh-blue'
              type = 'search' placeholder ='search for your robots here'
+             onChange={searchChange}
              />
         </div>
     );
